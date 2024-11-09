0306a6d22bc9baf5c477e7194e9661b6325c018c
kashanmobile <amjidtanoli@gmail.com>
2024-11-09 16:06:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function addToCart() {
+  alert("Product added to cart!");
+}
+
+function login() {
+  alert("Login form coming soon!");
+}
+
+function checkout() {
+  alert("Proceeding to checkout...");
+}
